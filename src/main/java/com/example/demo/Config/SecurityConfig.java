4abdd2ac30187a5bae4db8a0484ab20bb12aa718
Thajeevan <161084799+ThajeevanSE@users.noreply.github.com>
2025-11-21 23:09:44 +0530

    devps....

--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.example.demo.Config;
 
+
+import lombok.Data;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Data
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+
+        http.authorizeHttpRequests(auth->auth
+                .requestMatchers("api/auth/register", "/api/auth/login").permitAll()
+                .requestMatchers("api/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        );
+        http.httpBasic(Customizer.withDefaults());
+    return  http.build();
+    }
 }
+
